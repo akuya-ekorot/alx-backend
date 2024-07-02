e4fbd06ed08b20e69ed576c3ac58c138cd444852
Akuya Ekorot <akuyaekorot@gmail.com>
2024-07-02 17:10:55 +0300

    (0x02) - basic babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Basic flask app
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel, BabelConfiguration
+
+
+class Config(BabelConfiguration):
+    """config for the flask app"""
+
+    LANGUAGES = ["en", "fr"]
+
+    default_locale = "en"
+    default_timezone = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def home() -> str:
+    """Home route"""
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
