7469d6d3d36fb0d4354a16aebe4e7dffa3ffe860
Akuya Ekorot <akuyaekorot@gmail.com>
2024-07-02 16:32:40 +0300

    (0x02): basic flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Basic flask app
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home() -> str:
+    """Home route"""
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
